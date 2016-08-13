265c510ea49f4f67aa990bbf7de4447411507b64
Heiko-Waldschmidt <heiko.waldschmidt@unitybox.de>
2016-08-13 22:04:59 +0200

    Issue #9: Added "do you really want to delete the character dialogue"

---
+++ src/main/resources/static/js/confirm.js
@@ -0,0 +1,21 @@
+// from https://stackoverflow.com/questions/9334636/javascript-yes-no-alerts
+$(document).on('click', ':not(form)[data-confirm]', function(e){
+    if(!confirm($(this).data('confirm'))){
+        e.stopImmediatePropagation();
+        e.preventDefault();
+    }
+});
+
+$(document).on('submit', 'form[data-confirm]', function(e){
+    if(!confirm($(this).data('confirm'))){
+        e.stopImmediatePropagation();
+        e.preventDefault();
+    }
+});
+
+$(document).on('input', 'select', function(e){
+    var msg = $(this).children('option:selected').data('confirm');
+    if(msg != undefined && !confirm(msg)){
+        $(this)[0].selectedIndex = 0;
+    }
+});
