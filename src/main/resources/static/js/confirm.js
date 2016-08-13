99cf7f90a710046dfaf987aae2f51adcb783a9f3
Heiko-Waldschmidt <heiko.waldschmidt@unitybox.de>
2016-08-13 22:07:24 +0200

    Issue #9: Added docu

--- src/main/resources/static/js/confirm.js
+++ src/main/resources/static/js/confirm.js
@@ -19,3 +19,22 @@ $(document).on('input', 'select', function(e){
         $(this)[0].selectedIndex = 0;
     }
 });
+
+/* USAGE
+<!-- hyperlink example -->
+<a href="http://www.example.com" data-confirm="Are you sure you want to load this URL?">Anchor</a>
+
+<!-- button example -->
+<button type="button" data-confirm="Are you sure you want to click the button?">Button</button>
+
+<!-- form example -->
+<form action="http://www.example.com" data-confirm="Are you sure you want to submit the form?">
+    <button type="submit">Submit</button>
+</form>
+
+<!-- select option example -->
+<select>
+    <option>Select an option:</option>
+    <option data-confirm="Are you want to select this option?">Here</option>
+</select>
+*/
